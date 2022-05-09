d00fc1c540ac5512e6ecfbff799850067e435fe5
Marouan <marouan11hammich@gmail.com>
2022-05-09 20:47:12 +0200

    Falta consultar bien la bd

--- app/src/main/java/com/example/mufinds/MainActivity.java
+++ app/src/main/java/com/example/mufinds/MainActivity.java
@@ -3,12 +3,58 @@ package com.example.mufinds;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String URL = "https://mufinds.000webhostapp.com/validar_usuario.php";
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+    }
+
+    public void onClick (View view) {
+        StringRequest request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (!response.isEmpty()) {
+                    textView.setText("response");
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Usuario o contraseña no existen", Toast.LENGTH_SHORT);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_SHORT);
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> parametros = new HashMap<>();
+                parametros.put("usuario", "nereaLopez");
+                parametros.put("contraseña", "lolita123");
+                return parametros;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(request);
     }
 }
