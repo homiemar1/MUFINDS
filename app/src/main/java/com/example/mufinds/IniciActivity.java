da51d09cebcea1295a2848b4968e60385e6ba2c6
nerea2102 <lopeznereains@gmail.com>
2022-05-12 20:49:30 +0200

    configuraciones de menu

--- app/src/main/java/com/example/mufinds/IniciActivity.java
+++ app/src/main/java/com/example/mufinds/IniciActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 
 public class IniciActivity extends AppCompatActivity {
--- app/src/main/java/com/example/mufinds/PrincipalActivity.java
+++ app/src/main/java/com/example/mufinds/PrincipalActivity.java
@@ -2,10 +2,16 @@ package com.example.mufinds;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import java.sql.PreparedStatement;
 
 public class PrincipalActivity extends AppCompatActivity {
 
@@ -13,7 +19,44 @@ public class PrincipalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
-        ActionBar actionBar = getSupportActionBar();
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+
+    }
+
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_toolbar_menu, menu);
+        return true;
+    }*/
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_configuration:
+                Intent intent  = new Intent(this, ConfiguracionActivity.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.action_cambiarTema:
+                Toast.makeText(this, "Has seleccionado cambiar tema", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_logout:
+                Intent i1 = new Intent(this, IniciActivity.class);
+                startActivity(i1);
+                return true;
+
+            case R.id.action_informacio:
+                Toast.makeText(this, "Has seleccionado informacion", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_exit:
+                finishAffinity();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void onClickEditarPerfil(View view) {
