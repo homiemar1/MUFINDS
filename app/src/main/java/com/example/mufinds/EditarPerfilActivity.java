dc0bc33fb394092b71274cf2c2fdd770b1e60c30
Marouan <marouan11hammich@gmail.com>
2022-05-17 20:15:20 +0200

    Fotos de perfil

--- app/src/main/java/com/example/mufinds/EditarPerfilActivity.java
+++ app/src/main/java/com/example/mufinds/EditarPerfilActivity.java
@@ -1,20 +1,35 @@
 package com.example.mufinds;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 public class EditarPerfilActivity extends AppCompatActivity {
-    Spinner sp_editar_genero;
+    private Spinner sp_editar_genero;
+    private ImageView ivFotoPerfilEditarPerfil;
+    private Uri imageUri;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_perfil);
 
-        sp_editar_genero = findViewById(R.id.sp_editar_genero);
+        ivFotoPerfilEditarPerfil = findViewById(R.id.ivFotoPerfilEditarPerfil);
+        ivFotoPerfilEditarPerfil.setImageResource(R.drawable.fotoperfil);
+
+        sp_editar_genero = findViewById(R.id.spEditarGeneroEditarPerfil);
         String[] datos = new String[] {"Mujer", "Hombre", "Prefiero no contestar", "No binario"};
         sp_editar_genero.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item,datos));
 
@@ -23,4 +38,22 @@ public class EditarPerfilActivity extends AppCompatActivity {
     public void onClickAceptar(View view) {
         finish();
     }
+
+    public void onClickFotoDePerfil (View view) {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        someActivityResultLauncher.launch(gallery);
+    }
+
+    ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent intent = result.getData();
+                        imageUri = intent.getData();
+                        ivFotoPerfilEditarPerfil.setImageURI(imageUri);
+                    }
+                }
+            });
 }
--- app/src/main/java/com/example/mufinds/RegistroActivity2.java
+++ app/src/main/java/com/example/mufinds/RegistroActivity2.java
@@ -1,17 +1,56 @@
 package com.example.mufinds;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageView;
 
 public class RegistroActivity2 extends AppCompatActivity {
     private Intent intent;
+    private ImageView ivFotoPerfilRegistro;
+    private Uri imageUri;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro2);
+
+        ivFotoPerfilRegistro = findViewById(R.id.ivFotoPerfilRegistro);
+        ivFotoPerfilRegistro.setImageResource(R.drawable.fotoperfil);
+    }
+
+    public void onClickFotoDePerfil (View view) {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        someActivityResultLauncher.launch(gallery);
+    }
+
+    ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent intent = result.getData();
+                        imageUri = intent.getData();
+                        ivFotoPerfilRegistro.setImageURI(imageUri);
+                    }
+                }
+            });
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        imageUri = data.getData();
+        ivFotoPerfilRegistro.setImageURI(imageUri);
     }
 
     public void onClickAtras (View view) {
