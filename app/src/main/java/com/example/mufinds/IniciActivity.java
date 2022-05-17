dffadf0b9bcd6edd60e264f04a2db367cb1e7eab
Marouan <marouan11hammich@gmail.com>
2022-05-17 19:28:33 +0200

    Logo añadido en la pantalla principal

--- app/src/main/java/com/example/mufinds/IniciActivity.java
+++ app/src/main/java/com/example/mufinds/IniciActivity.java
@@ -7,14 +7,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ImageView;
 
 public class IniciActivity extends AppCompatActivity {
-
+    ImageView ivLogoInicio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inici);
 
+        ivLogoInicio = findViewById(R.id.ivLogoInicio);
+        ivLogoInicio.setImageResource(R.drawable.logomufinds);
     }
 
     public void onClickRegistrar (View view) {
