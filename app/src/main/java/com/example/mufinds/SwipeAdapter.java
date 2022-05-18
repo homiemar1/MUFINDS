ddc25450569b3635513ad3ac6e2792430a53aa7e
Marouan <marouan11hammich@gmail.com>
2022-05-18 17:49:44 +0200

    swipe card casi terminada

---
+++ app/src/main/java/com/example/mufinds/SwipeAdapter.java
@@ -0,0 +1,48 @@
+package com.example.mufinds;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.List;
+
+public class SwipeAdapter extends BaseAdapter {
+    private Context context;
+    private List<Integer> list;
+
+    public SwipeAdapter(Context context, List<Integer> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view;
+
+        if (convertView == null) {
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_koloda, parent, false);
+        }
+        else {
+            view =  convertView;
+        }
+
+        return view;
+    }
+}
