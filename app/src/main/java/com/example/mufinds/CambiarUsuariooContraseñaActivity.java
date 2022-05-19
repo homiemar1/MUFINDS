1700161dae007d4d1ecfec8d5902d42f26e347dd
Marouan <marouan11hammich@gmail.com>
2022-05-19 20:44:15 +0200

    update editar nombre usuario

--- app/src/main/java/com/example/mufinds/CambiarUsuariooContraseñaActivity.java
+++ app/src/main/java/com/example/mufinds/CambiarUsuariooContraseñaActivity.java
@@ -1,27 +1,44 @@
 package com.example.mufinds;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class CambiarUsuariooContraseñaActivity extends AppCompatActivity {
-    TextView tvTituloCambio;
-    EditText etDatoAntiguo, etDatoNuevo, etDatoConfirmacion;
+    private TextView tvTituloCambio;
+    private EditText etDatoAntiguo, etDatoNuevo, etDatoConfirmacion;
+    private int valor;
+    private FirebaseFirestore database;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cambiar_usuario);
+
+        database = FirebaseFirestore.getInstance();
 
         tvTituloCambio = findViewById(R.id.tvTituloCambio);
         etDatoAntiguo = findViewById(R.id.etDatoAntiguo);
         etDatoNuevo = findViewById(R.id.etDatoNuevo);
         etDatoConfirmacion = findViewById(R.id.etDatoConfirmacion);
 
-        int valor = getIntent().getIntExtra("variable", 1);
+        valor = getIntent().getIntExtra("variable", 1);
         if (valor == 1) {
             tvTituloCambio.setText("CAMBIAR NOMBRE DE USUARIO");
             etDatoAntiguo.setHint("Usuario");
@@ -49,23 +66,64 @@ public class CambiarUsuariooContraseñaActivity extends AppCompatActivity {
 
         if ("".equals(datoAntiguo)) {
             etDatoAntiguo.setError("Introduce el dato antiguo");
+            return;
         }
         else if ("".equals(datoNuevo)) {
             etDatoNuevo.setError("Introduce el dato nuevo");
+            return;
         }
         else if ("".equals(datoConfirmacion)) {
             etDatoConfirmacion.setError("Introduce el dato confirmatorio");
+            return;
+        }
+
+        //comprobar datos
+        if (valor == 1) {
+            consultaExistencia(datoAntiguo, datoNuevo);
         }
         else {
-            //comprobar datos
-
-            //si se puede hacer el cambio
-            finish();
-            //si no, mensaje de que no coinciden
-
 
         }
+        //si se puede hacer el cambio
+        finish();
+        //si no, mensaje de que no coinciden
 
+    }
+
+    public void consultaExistencia(String datoAntiguo, String datoNuevo) {
+        database.collection("users").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        if (document.getId().equals(datoAntiguo)) {
+                            updateNombreUsuario(datoNuevo);
+                        }
+                        else {
+                            etDatoAntiguo.setError("Usuario no existe");
+                        }
+                    }
+                } else {
+                    System.out.println("Error getting documents." + task.getException());
+                }
+            }
+        });
+    }
+
+    public void updateNombreUsuario(String datoNuevo) {
+        database.collection("users").document("nerea").update("ID", datoNuevo)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(CambiarUsuariooContraseñaActivity.this, "Update satisfactorio", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(CambiarUsuariooContraseñaActivity.this, "Updatn't satisfactorio", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     public class AsteriskPasswordTransformationMethod extends PasswordTransformationMethod {
