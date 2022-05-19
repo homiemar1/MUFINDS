0bb9996eeddbc459ba4b1ed2e1b4838218d312a8
Marouan <marouan11hammich@gmail.com>
2022-05-19 19:53:07 +0200

    cosass del listview

--- app/src/main/java/com/example/mufinds/AmigosList.java
+++ app/src/main/java/com/example/mufinds/AmigosList.java
@@ -5,20 +5,32 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.util.ArrayUtils;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class AmigosList extends ArrayAdapter {
     private String[] userNames;
-    private Integer imageid;
+    private String[] cancionComun;
+    private Integer perfilId;
     private Activity context;
 
-    public AmigosList(Activity context, String[] countryNames, Integer imageid) {
-        super(context, R.layout.activity_amistades, countryNames);
-        this.context = context;
-        this.userNames = countryNames;
-        this.imageid = imageid;
 
+    public AmigosList(Activity context, String[] userNames, String[] cancionComun, Integer perfilId) {
+        super(context, R.layout.activity_amistades, userNames);
+        this.context = context;
+        this.userNames = userNames;
+        this.cancionComun = cancionComun;
+        this.perfilId = perfilId;
     }
 
     @Override
@@ -27,11 +39,15 @@ public class AmigosList extends ArrayAdapter {
         LayoutInflater inflater = context.getLayoutInflater();
         if(convertView==null)
             row = inflater.inflate(R.layout.row_item, null, true);
-        TextView textViewCountry = (TextView) row.findViewById(R.id.userName);
-        ImageView imageFlag = (ImageView) row.findViewById(R.id.imgError);
 
-        textViewCountry.setText(userNames[position]);
-        imageFlag.setImageResource(imageid);
+        TextView textViewUsuario = (TextView) row.findViewById(R.id.userName);
+        TextView textViewCancionesComun = (TextView) row.findViewById(R.id.tvCancionesComunAmigosList);
+        ImageView imageViewUsuario = (ImageView) row.findViewById(R.id.fotoPerfilAmigosList);
+
+        textViewUsuario.setText(userNames[position]);
+        textViewCancionesComun.setText(cancionComun[position]);
+        imageViewUsuario.setImageResource(perfilId);
+
         return row;
     }
 }
--- app/src/main/java/com/example/mufinds/AmistadesActivity.java
+++ app/src/main/java/com/example/mufinds/AmistadesActivity.java
@@ -2,7 +2,11 @@ package com.example.mufinds;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -12,28 +16,93 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class AmistadesActivity extends AppCompatActivity {
-    ListView lvSolicitudesAmistad, lvAmigos;
+    private AmigosList amigosList;
+    private ListView lvSolicitudesAmistad, lvAmigos;
+    private String[] users;
+    String[] cancionesEnComun;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_amistades);
 
+        users = new String[]{"enMic", "tu_xulo_99", "tu_jenny_fav"};
+        cancionesEnComun = new String[]{"20 canciones en comun", "2 canciones en comun", "0 canciones en comun"};
+
         lvSolicitudesAmistad = findViewById(R.id.lvSolicitudesAmistad);
-        lvAmigos = findViewById(R.id.lvAmigos);
 
-        String[] users = new String[]{"tu", "madre"};
+        Integer fotoPerfil = R.drawable.fotoperfil;
 
-        Integer imgs = R.drawable.error;
-
-        AmigosList customCountryList = new AmigosList(this, users, imgs);
-        lvSolicitudesAmistad.setAdapter(customCountryList);
-
+        //Solicitudes de amistad
+        amigosList = new AmigosList(this, users, cancionesEnComun, fotoPerfil);
+        lvSolicitudesAmistad.setAdapter(amigosList);
         lvSolicitudesAmistad.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                Toast.makeText(getApplicationContext(),"You Selected " + users[position-1] + " as Country",Toast.LENGTH_SHORT).show();
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+                //añadir usuario
+                AlertDialog.Builder alert = new AlertDialog.Builder(AmistadesActivity.this);
+                alert.setTitle("Añadir amistad");
+                alert.setMessage("Estas segur@ de que quieres añadir a este usuario?")
+                        .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                alert.show();
+                                users = removeElement(users, position);
+                                cancionesEnComun = removeElement(cancionesEnComun, position);
+                                amigosList.remove(position);
+                                amigosList.notifyDataSetChanged();
+                            }
+                        }).setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                });
+
             }
         });
 
+        //Amigos actuales (hace falta cambiar las canciones en comun por una descripcion)
+        amigosList = new AmigosList(this, users, cancionesEnComun, fotoPerfil);
+        lvAmigos = findViewById(R.id.lvAmigos);
+        lvAmigos.setAdapter(amigosList);
+        lvAmigos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+                //eliminar amigo
+                AlertDialog.Builder alert = new AlertDialog.Builder(AmistadesActivity.this);
+                alert.setTitle("Eliminar amistad");
+                alert.setMessage("Estas segur@ de que quieres eliminar de tus contactos a este usuario? Esta acción será permanente")
+                        .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                }).setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+
+                            }
+                        });
+                alert.show();
+            }
+        });
+    }
+
+    // remove element method
+    public String[] removeElement(String[] arrGiven, int index)
+    {
+        // if empty
+        if(arrGiven == null || index < 0 || index >= arrGiven.length)
+        {
+            return arrGiven;
+        }
+        // creating another array one less than initial array
+        String[] newArray = new String[arrGiven.length - 1];
+        // copying elements except index
+        for(int a = 0, b = 0; a < arrGiven.length; a++)
+        {
+            if(a == index)
+            {
+                continue;
+            }
+            newArray[b++] = arrGiven[a];
+        }
+        return newArray;
     }
 }
