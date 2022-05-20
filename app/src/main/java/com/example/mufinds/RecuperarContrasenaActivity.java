6e61ef47cc1ce05aced96b212f85854d1f69cc5f
nerea <lopeznereains@gmail.com>
2022-05-20 15:15:33 +0200

    recuperar usuario y contraseña funcionan

--- app/src/main/java/com/example/mufinds/RecuperarContrasenaActivity.java
+++ app/src/main/java/com/example/mufinds/RecuperarContrasenaActivity.java
@@ -1,5 +1,6 @@
 package com.example.mufinds;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,14 +8,24 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 public class RecuperarContrasenaActivity extends AppCompatActivity {
     EditText etNombreUsuarioRecuperarContraseña, etNombreRecuperarContraseña, etApellidosRecuperarContraseña;
     TextView tvRespuestaRecuperarContraseña;
     EditText etdFechaNacimientoRecuperarContraseña;
+    String nombreUsuario, nombre, apellidos;
+    FirebaseFirestore database;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recuperar_contrasena);
+        database = FirebaseFirestore.getInstance();
 
         etNombreUsuarioRecuperarContraseña = findViewById(R.id.etNombreUsuarioRecuperarContraseña);
         etNombreRecuperarContraseña = findViewById(R.id.etNombreRecuperarContraseña);
@@ -27,11 +38,12 @@ public class RecuperarContrasenaActivity extends AppCompatActivity {
     public void onClickRecuperar(View view) {
         //comprobar que sea igual al de la base de datos
 
-        String nombrUsuario = etNombreUsuarioRecuperarContraseña.getText().toString();
-        String nombre = etNombreRecuperarContraseña.getText().toString();
-        String apellidos = etApellidosRecuperarContraseña.getText().toString();
+        nombreUsuario = etNombreUsuarioRecuperarContraseña.getText().toString();
+        nombre = etNombreRecuperarContraseña.getText().toString();
+        apellidos = etApellidosRecuperarContraseña.getText().toString();
 
-        if ("".equals(nombrUsuario)) {
+
+        if ("".equals(nombreUsuario)) {
             etNombreUsuarioRecuperarContraseña.setError("Introduce tu nombre de usuario");
         }
         else if ("".equals(nombre)) {
@@ -40,10 +52,31 @@ public class RecuperarContrasenaActivity extends AppCompatActivity {
         else if ("".equals(apellidos)) {
             etApellidosRecuperarContraseña.setError("Introduce tu nombre de usuario");
         }
+        else {
+            database.collection("users").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            System.out.println(document.getId() + " => " + document.getData());
+                            if (document.getId().equals(nombreUsuario)) {
+                                String dbNombre = document.getData().get("nombre").toString();
+                                String dbApellidos = document.getData().get("apellido").toString();
+                                if (dbNombre.equals(nombre) && dbApellidos.equals(apellidos)) {
+                                    tvRespuestaRecuperarContraseña.setText("Tu contraseña es: " + document.getData().get("password").toString());
+                                }
+                                else {
+                                    tvRespuestaRecuperarContraseña.setText("Los datos no coinciden. Vuelva a intentarlo");
+                                }
 
-        //si coincide
-        tvRespuestaRecuperarContraseña.setText("Tu contraseña es: " + "variable de contraseña");
-        //si no
-        tvRespuestaRecuperarContraseña.setText("Los datos no coinciden. Vuelva a intentarlo");
+                            }
+                        }
+                    } else {
+                        System.out.println("Error getting documents." + task.getException());
+                    }
+                }
+            });
+        }
+
     }
 }
--- app/src/main/java/com/example/mufinds/RecuperarUsuarioActivity.java
+++ app/src/main/java/com/example/mufinds/RecuperarUsuarioActivity.java
@@ -1,20 +1,36 @@
 package com.example.mufinds;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 public class RecuperarUsuarioActivity extends AppCompatActivity {
     EditText etEmailRecuperarUsuario, etContraseñaRecuperarUsuario;
     TextView tvRespuestaRecuperarUsuario;
+    SharedPreferences sharedPref;
+    FirebaseFirestore database;
+
+    String contraseña, email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recuperar_usuario);
+        sharedPref = getSharedPreferences(getString(R.string.preferences), Context.MODE_PRIVATE);
+        database = FirebaseFirestore.getInstance();
 
         etEmailRecuperarUsuario = findViewById(R.id.etEmailRecuperarUsuario);
         etContraseñaRecuperarUsuario = findViewById(R.id.etContraseñaRecuperarUsuario);
@@ -24,19 +40,45 @@ public class RecuperarUsuarioActivity extends AppCompatActivity {
 
     public void onClickRecuperar(View view) {
         //comprobar que sea igual al de la base de datos
-        String email = etEmailRecuperarUsuario.getText().toString();
-        String contraseña = etContraseñaRecuperarUsuario.getText().toString();
+        email = etEmailRecuperarUsuario.getText().toString();
+        contraseña = etContraseñaRecuperarUsuario.getText().toString();
 
         if ("".equals(email)) {
             etEmailRecuperarUsuario.setError("Introduce tu email");
+            return;
         }
         else if ("".equals(contraseña)) {
             etContraseñaRecuperarUsuario.setError("Introduce tu contraseña");
+            return;
+        }
+        else {
+            String nombreUsuario = sharedPref.getString("nombreUsuario","");
+
+            database.collection("users").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            System.out.println(document.getId() + " => " + document.getData());
+                            String dbEmail = document.getData().get("email").toString();
+                            if (dbEmail.equals(email)) {
+                                String dbPassword = document.getData().get("password").toString();
+                                String usuario = document.getData().get("nombreUsuari").toString();
+                                if (dbPassword.equals(contraseña) && dbEmail.equals(email)) {
+                                    tvRespuestaRecuperarUsuario.setText("Tu nombre de usuario es: " + usuario);
+                                }
+                                else {
+                                    tvRespuestaRecuperarUsuario.setText("Los datos no coinciden. Vuelva a intentarlo");
+                                }
+
+                            }
+                        }
+                    } else {
+                        System.out.println("Error getting documents." + task.getException());
+                    }
+                }
+            });
         }
 
-        //si coincide
-        tvRespuestaRecuperarUsuario.setText("Tu nombre de usuario es: " + "variable de usuario");
-        //si no
-        tvRespuestaRecuperarUsuario.setText("Los datos no coinciden. Vuelva a intentarlo");
     }
 }
