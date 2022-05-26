c89e3694fafaa786d0ef2124c51a98ea5ad1696e
nerea2102 <lopeznereains@gmail.com>
2022-05-26 16:37:32 +0200

    nada

--- app/src/main/java/com/example/mufinds/AmistadesActivity.java
+++ app/src/main/java/com/example/mufinds/AmistadesActivity.java
@@ -6,16 +6,10 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.graphics.Paint;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
 
 public class AmistadesActivity extends AppCompatActivity {
     TextView tvSolicitudes,tvAmigos;
